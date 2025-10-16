e543f57fafe4544db801d5bdf707f0d28ac1a229
MartTave <55973969+MartTave@users.noreply.github.com>
2025-10-16 13:07:19 +0200

    Wip

--- train_model.py
+++ train_model.py
@@ -1,30 +1,55 @@
+from multiprocessing import Pool
 
 import numpy as np
 import torch
 import torch.nn as nn
-from torch.utils.data import TensorDataset, DataLoader, random_split
+from matplotlib import image
+from torch.utils.data import DataLoader, TensorDataset, random_split
+
 from model.model import AlexNetAtHome
 
 # Set random seeds for reproducibility
 torch.manual_seed(42)
 np.random.seed(42)
 
-# Load the dataset
-with np.load('dataset/dataset_1.npz') as data:
-    valid_pairs = data['valid_pairs']
-    invalid_pairs = data['invalid_pairs']
+
+def load_image_pair(pair):
+    return image.imread(pair[0]), image.imread(pair[1])
+
+
+valid_image_pairs = None
+invalid_image_pairs = None
+with Pool(50) as p:
+    # Load the dataset
+    with np.load("dataset/dataset_1.npz") as data:
+        valid_pairs = data["valid_pairs"]
+        invalid_pairs = data["invalid_pairs"]
+    print("Images paths loaded")
+    valid_image_pairs = p.map(load_image_pair, valid_pairs)
+    invalid_image_pairs = p.map(load_image_pair, invalid_pairs)
+
+    print("Images loaded")
+
 
 # Create labels
-valid_labels = np.zeros(len(valid_pairs))
-invalid_labels = np.ones(len(invalid_pairs))
+valid_labels = np.zeros(len(valid_image_pairs))
+invalid_labels = np.ones(len(invalid_image_pairs))
+
+print("Labels created")
 
 # Combine the data and labels
-X = np.concatenate((valid_pairs, invalid_pairs), axis=0)
+X = np.concatenate((valid_image_pairs, invalid_image_pairs), axis=0)
+valid_image_pairs = None
+invalid_image_pairs = None
 y = np.concatenate((valid_labels, invalid_labels), axis=0)
+
+print("Arrays created")
 
 # Convert to PyTorch tensors
 X_tensor = torch.tensor(X, dtype=torch.float32)
 y_tensor = torch.tensor(y, dtype=torch.float32).view(-1, 1)
+
+print("Tensor created")
 
 # Create a dataset
 dataset = TensorDataset(X_tensor, y_tensor)
@@ -59,7 +84,9 @@ for epoch in range(num_epochs):
         optimizer.step()
         running_loss += loss.item()
 
-    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(train_loader):.4f}')
+    print(
+        f"Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss / len(train_loader):.4f}"
+    )
 
     # Validation
     model.eval()
@@ -72,8 +99,8 @@ for epoch in range(num_epochs):
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    print(f'Accuracy on test set: {100 * correct / total:.2f}%')
+    print(f"Accuracy on test set: {100 * correct / total:.2f}%")
 
 # Save the model
-torch.save(model.state_dict(), 'trained_model.pth')
-print('Finished Training and saved the model to trained_model.pth')
+torch.save(model.state_dict(), "trained_model.pth")
+print("Finished Training and saved the model to trained_model.pth")
